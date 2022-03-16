7ba45d1890300c08b81d665fecceedf3ae98805e
alfinikma <alfinikmaagustin@gmail.com>
2022-03-16 22:28:11 +0700

    parcel size

---
+++ lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:send_parcel_screen/parcel_app_theme.dart';
+import 'package:send_parcel_screen/ui/screens/screens.dart';
+import 'package:send_parcel_screen/ui/widgets/widgets.dart';
+
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ParcelAppTheme.lightTheme,
+      home:  Scaffold(
+        body:  SendParcelScreen(),
+        bottomNavigationBar: MyBottomNavigationBar(),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:send_parcel_screen/app.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
